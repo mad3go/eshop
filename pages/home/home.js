418d5ace935c454d6f8815ce3a946fab275edb6b
mad3go <boydor808@gmail.com>
2022-03-15 00:32:26 +0800

    更新了home页面，新增order页面和tabs组件的内容

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,8 @@
+//var timestamp = require('../../utils/timestamp.js') //引入进行格式化时间
+//先将时间转换成时间戳
+//var stamp = timestamp.getDateTimeStamp(teacherAppraiseTime)
+//然后将时间戳转成几个小时、几分钟等格式
+//var time = timestamp.getDateDiff(stamp);
 // pages/home/home.js
 Page({
 
@@ -5,8 +10,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    antiepd_info_list:[{
+      img:'../../icon/sucai.jpeg', title:'截至21日8时，上海累计排查管控3853人核酸检测均阴性', source:'绿政公署', time:50+'分钟前' ,unique:'page1'
+    },{
+      img:'../../icon/roupian.jpeg', title:'国家卫健委：昨日新增本土确诊93例、本土无症状30例', source:'绿政公署', time:50+'分钟前' ,unique:'page2'
+    },{
+      img:'../../icon/roupian.jpeg', title:'武汉新增10例阳性感染者，均与培训班相关', source:'绿政公署', time:50+'分钟前' ,unique:'page3'
+    }]
   },
+  
 
   /**
    * 生命周期函数--监听页面加载
