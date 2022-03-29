fcca776bc9abb2be94db734cb225631e64130beb
mad3go <boydor808@gmail.com>
2022-03-29 17:59:34 +0800

    增加了防疫专题页面

--- pages/antiepd/antiepd.js
+++ pages/antiepd/antiepd.js
@@ -6,11 +6,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-    autoplay: true,
-    interval: 5000,
-    duration: 500,  //有接口后这三个参数删掉
     //轮播图数组
-    swiperList:[]
+    swiperList:[
+      {
+        id:0, image:'../../icon/k1.jpeg'
+    },{
+      id:1, image:'../../icon/k2.jpeg'
+    },{
+      id:2, image:'../../icon/k3.jpeg'
+    }],
+    tabs:[
+      {
+        id:0,
+        name:"最新报道",
+        isActive:true
+      },{
+        id:1,
+        name:"肺炎知识",
+        isActive:false
+      }
+    ],
+    antiepd_info_list:[{
+      img:'../../icon/sucai.jpeg', title:'截至21日8时，上海累计排查管控3853人核酸检测均阴性', source:'央视新闻', time:50+'分钟前' ,unique:'page1'
+    },{
+      img:'../../icon/roupian.jpeg', title:'国家卫健委：昨日新增本土确诊93例、本土无症状30例', source:'绿政公署', time:50+'分钟前' ,unique:'page2'
+    },{
+      img:'../../icon/roupian.jpeg', title:'武汉新增10例阳性感染者，均与培训班相关', source:'真理部', time:50+'分钟前' ,unique:'page3'
+    }],
+    covid_increase:[{
+      inner:4, city:[{
+        cname:"上海", nums:"3"
+      },{
+        cname:"辽宁", nums:"1"
+      }]
+    },{
+      outer:9, input:9, nowNums:2903
+    },{
+    }],
+    swiperCurrent:0
   },
 
   /**
@@ -19,12 +52,27 @@ Page({
   onLoad: function (options) {
     this.getSwiperList;
   },
-    getSwiperList(){
-      request({url:""})
-      .then(result => {
-        this.setData({
-          swiperList:result.data.message
-        })
+
+  getSwiperList(){
+    request({url:""})
+    .then(result => {
+      this.setData({
+        swiperList:result.data.message
       })
-    }
+    })
+  },
+  handleItemChange(e) {
+    //接受子组件传递过来的参数
+    const {index} = e.detail;
+    let {tabs} = this.data;
+    tabs.forEach((v,i) => i === index ? v.isActive = true : v.isActive = false);
+    this.setData({
+      tabs
+    })
+  },
+  handleSwiperChange(e){
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
+  },
 })
